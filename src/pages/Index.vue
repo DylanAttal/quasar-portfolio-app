3dd4647d03f5a3b145c060e214057fd843b211ee
Dylan Attal <attal.dylan@gmail.com>
2020-02-11 18:43:21 -0500

    Adds images. Adds profile pic, name, and job title to top section.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,17 @@
 <template>
   <q-page class="flex flex-center">
-    <section id="top" class="bg-teal-4">
-      <h2>top</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-        excepturi, illum nesciunt accusamus qui sunt nam voluptate cum doloribus
-        consequatur rerum animi consectetur inventore veniam alias labore
-        quaerat aperiam nostrum!
+    <section
+      id="top"
+      class="column justify-center items-center full-width bg-teal-4"
+    >
+      <img
+        src="../assets/prof-cropped.jpeg"
+        class="profile-picture q-mb-xl"
+        alt="A picture of Dylan's face smiling."
+      />
+      <p class="text-h2 text-white text-weight-bolder">DYLAN ATTAL</p>
+      <p class="text-h3 text-white text-weight-bold">
+        Full Stack Software Engineer
       </p>
     </section>
     <section id="portfolio">
@@ -57,5 +62,11 @@ export default {
 <style lang="scss">
 section {
   min-height: calc(100vh - 62px);
+}
+
+.profile-picture {
+  height: 300px;
+  width: 280px;
+  border-radius: 200px;
 }
 </style>
